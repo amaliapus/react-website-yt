1f84558efb33f3ee406e24c7893df2f3c5349e8f
amaliapus <amaliapus.9d@gmail.com>
2022-06-20 10:04:01 +0700

    finishing

---
+++ src/components/Signin/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, FormWrap, Icon, FormContent, Form, FormH1, FormLabel, FormInput, FormButton, Text } from './SigninElements';
+
+function SignIn() {
+    return (
+        <>
+            <Container>
+                <FormWrap>
+                    <Icon to='/'>Camellia</Icon>
+                    <FormContent>
+                        <Form >
+                            <FormH1>Sign in to your account</FormH1>
+                            <FormLabel htmlFor='email'>Email</FormLabel>
+                            <FormInput type='email' id='email' required />
+                            <FormLabel htmlFor='password'>Password</FormLabel>
+                            <FormInput type='password' id='password' required />
+                            <FormButton type='submit'>Continue</FormButton>
+                            <Text>Forgot Password</Text>
+                        </Form>
+                    </FormContent>
+                </FormWrap>
+            </Container>
+        </>
+    );
+};
+
+export default SignIn;
